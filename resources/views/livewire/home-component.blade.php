06c7e1a600d397c8525e8febbf79182b84dd1259
Hamza Umar <humar6078@gmail.com>
2021-10-29 06:46:37 +0800

    initial commit

---
+++ resources/views/livewire/home-component.blade.php
@@ -0,0 +1,280 @@
+<div class="home">
+    <div class="home_slider_container">
+        <div class="owl-carousel owl-theme home_slider">
+            <div class="owl-item home_slider_item">
+                <div class="home_slider_background" style="background-image:url(assets/images/banner/_banner1.jpg)"></div>
+                <div class="home_slider_content_container">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col">
+                                <div class="home_slider_content" data-animation-in="fadeIn" data-animation-out="animate-out fadeOut">
+                                    <div class="home_slider_title">A new Online Shop experience.</div>
+                                    <div class="home_slider_subtitle">Lorem ipsum dolor sit amet, consectetur
+                                        adipiscing elit. Nullam a ultricies metus. Sed nec molestie eros. Sed
+                                        viverra velit venenatis fermentum luctus.</div>
+                                    <div class="button button_dark home_button"><a href="store.html">shop now <span class="pl-2"><img src="assets/images/svg/basket.svg" width="17" alt=""></span></a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="owl-item home_slider_item">
+                <div class="home_slider_background" style="background-image:url(assets/images/banner/_banner2.jpg)"></div>
+                <div class="home_slider_content_container">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col">
+                                <div class="home_slider_content" data-animation-in="fadeIn" data-animation-out="animate-out fadeOut">
+                                    <div class="home_slider_title">A new Online Shop experience.</div>
+                                    <div class="home_slider_subtitle">Lorem ipsum dolor sit amet, consectetur
+                                        adipiscing elit. Nullam a ultricies metus. Sed nec molestie eros. Sed
+                                        viverra velit venenatis fermentum luctus.</div>
+                                    <div class="button button_dark home_button"><a href="#">Shop Now</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="owl-item home_slider_item">
+                <div class="home_slider_background" style="background-image:url(assets/images/banner/_banner3.jpg)"></div>
+                <div class="home_slider_content_container">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col">
+                                <div class="home_slider_content" data-animation-in="fadeIn" data-animation-out="animate-out fadeOut">
+                                    <div class="home_slider_title">A new Online Shop experience.</div>
+                                    <div class="home_slider_subtitle">Lorem ipsum dolor sit amet, consectetur
+                                        adipiscing elit. Nullam a ultricies metus. Sed nec molestie eros. Sed
+                                        viverra velit venenatis fermentum luctus.</div>
+                                    <div class="button button_dark home_button"><a href="#">Shop Now</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="home_slider_dots_container">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="home_slider_dots">
+                            <ul id="home_slider_custom_dots" class="home_slider_custom_dots">
+                                <li class="home_slider_custom_dot active">01.</li>
+                                <li class="home_slider_custom_dot">02.</li>
+                                <li class="home_slider_custom_dot">03.</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+<div class="avds">
+    <div class="avds_container d-flex flex-lg-row flex-column align-items-start justify-content-between">
+        <div class="avds_small">
+            <div class="avds_background" style="background-image:url(assets/images/DSC02649.jpg)"></div>
+            <div class="avds_small_inner">
+                <div class="avds_discount_container">
+                    <img src="assets/images/xdiscount.png.pagespeed.ic.FxW5u5rCrb.webp" alt="">
+                    <div>
+                        <div class="avds_discount">
+                            <div>50<span>g</span></div>
+                            <div>grams</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="avds_small_content">
+                    <div class="avds_title"><span>Moringa</span> Fema</div>
+                    <div class="avds_link"><a href="categories.html">See More</a></div>
+                </div>
+            </div>
+        </div>
+        <div class="avds_large">
+            <div class="avds_background" style="background-image:url(assets/images/DSC02657.jpg)"></div>
+            <div class="avds_large_container">
+                <div class="avds_large_content">
+                    <div class="avds_title">Moringa pulver</div>
+                    <div class="avds_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a
+                        ultricies metus. Sed nec molestie eros. Sed viver ra velit venenatis fermentum luctus.
+                    </div>
+                    <div class="avds_link avds_link_large"><a href="categories.html">See More</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="products">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="product_grid">
+
+                    <div class="product">
+                        <div class="product_image"><img src="assets/images/DSC02633.jpg" alt=""></div>
+                        <div class="product_extra product_new"><a href="categories.html">New</a></div>
+                        <div class="product_content">
+                            <div class="product_title"><a href="product.html">Moringa fema</a></div>
+                            <div class="product_price">GHS 200.00</div>
+                        </div>
+                    </div>
+
+                    <div class="product">
+                        <div class="product_image"><img src="assets/images/DSC02635.jpg" alt=""></div>
+                        <!-- <div class="product_extra product_sale"><a href="categories.html">Sale</a></div> -->
+                        <div class="product_content">
+                            <div class="product_title"><a href="product.html">Moringa fema</a></div>
+                            <div class="product_price">GHS 200.00</div>
+                        </div>
+                    </div>
+
+                    <div class="product">
+                        <div class="product_image"><img src="assets/images/DSC02641.jpg" alt=""></div>
+                        <div class="product_content">
+                            <div class="product_title"><a href="product.html">Moringa fema</a></div>
+                            <div class="product_price">GHS 200.00</div>
+                        </div>
+                    </div>
+
+                    <div class="product">
+                        <div class="product_image"><img src="assets/images/DSC02649.jpg" alt=""></div>
+                        <div class="product_content">
+                            <div class="product_title"><a href="product.html">Moringa fema</a></div>
+                            <div class="product_price">GHS 200.00</div>
+                        </div>
+                    </div>
+
+                    <div class="product">
+                        <div class="product_image"><img src="assets/images/DSC02649.jpg" alt=""></div>
+                        <div class="product_content">
+                            <div class="product_title"><a href="product.html">Moringa fema</a></div>
+                            <div class="product_price">GHS 200.00</div>
+                        </div>
+                    </div>
+
+                    <div class="product">
+                        <div class="product_image"><img src="assets/images/DSC02649.jpg" alt=""></div>
+                        <div class="product_extra product_hot"><a href="categories.html">Hot</a></div>
+                        <div class="product_content">
+                            <div class="product_title"><a href="product.html">Moringa fema</a></div>
+                            <div class="product_price">GHS 200.00</div>
+                        </div>
+                    </div>
+
+                    <div class="product">
+                        <div class="product_image"><img src="assets/images/DSC02649.jpg" alt=""></div>
+                        <div class="product_content">
+                            <div class="product_title"><a href="product.html">Moringa fema</a></div>
+                            <div class="product_price">GHS 200.00</div>
+                        </div>
+                    </div>
+
+                    <div class="product">
+                        <div class="product_image"><img src="assets/images/DSC02649.jpg" alt=""></div>
+                        <div class="product_extra product_sale"><a href="categories.html">Hot</a></div>
+                        <div class="product_content">
+                            <div class="product_title"><a href="product.html">Moringa fema</a></div>
+                            <div class="product_price">GHS 200.00</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="avds_xl">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="avds_xl_container clearfix">
+                    <div class="avds_xl_background" style="background-image:url(images/xavds_xl.jpg.pagespeed.ic.hPLCuIxgvV.webp)"></div>
+                    <div class="avds_xl_content">
+                        <div class="avds_title">Amazing Devices</div>
+                        <div class="avds_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a
+                            ultricies metus.</div>
+                        <div class="avds_link avds_xl_link"><a href="categories.html">See More</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- <div class="icon_boxes">
+            <div class="container">
+                <div class="row icon_box_row">
+
+                    <div class="col-lg-4 icon_box_col">
+                        <div class="icon_box">
+                            <div class="icon_box_image"><img src="assets/images/svg/icon_1.svg" alt=""></div>
+                            <div class="icon_box_title">Free Shipping Worldwide</div>
+                            <div class="icon_box_text">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a ultricies metus.
+                                    Sed nec molestie.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 icon_box_col">
+                        <div class="icon_box">
+                            <div class="icon_box_image"><img src="assets/images/svg/icon_2.svg" alt=""></div>
+                            <div class="icon_box_title">Free Returns</div>
+                            <div class="icon_box_text">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a ultricies metus.
+                                    Sed nec molestie.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 icon_box_col">
+                        <div class="icon_box">
+                            <div class="icon_box_image"><img src="assets/images/svg/icon_3.svg" alt=""></div>
+                            <div class="icon_box_title">24h Fast Support</div>
+                            <div class="icon_box_text">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a ultricies metus.
+                                    Sed nec molestie.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> -->
+
+<!-- <div class="newsletter">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="newsletter_border"></div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-8 offset-lg-2">
+                        <div class="newsletter_content text-center">
+                            <div class="newsletter_title">Subscribe to our newsletter</div>
+                            <div class="newsletter_text">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a ultricies metus.
+                                    Sed nec molestie eros</p>
+                            </div>
+                            <div class="newsletter_form_container">
+                                <form action="#" id="newsletter_form" class="newsletter_form">
+                                    <input type="email" class="newsletter_input" required>
+                                    <button class="newsletter_button trans_200"><span>Subscribe</span></button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> -->
